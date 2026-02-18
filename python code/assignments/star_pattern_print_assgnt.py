701dc132942ae9693407c063ebbd3c4e292fb501
Luna1120 <79321421+Luna1120@users.noreply.github.com>
2026-02-18 11:42:29 +0530

    Add files via upload

---
+++ python code/assignments/star_pattern_print_assgnt.py
@@ -0,0 +1,34 @@
+from loguru import logger
+
+# Assignment question
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# print('* '*3)
+
+# for i in range(5):
+#     logger.info('* ' * (i+1))
+
+
+# Assignment question homework
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+a=0
+for i in range(5,10):
+    print('* ' * (i-a))
+    a=a+2
+
+# 5 to 10
+# 5 - ***** -0
+# 6 - ****  -2
+# 7 - ***   -4
+# 8 - **    -6
+# 9 - *     -8
